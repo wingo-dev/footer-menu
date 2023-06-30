929708e513167b237322255358914b878ec3c2c8
wingo-dev <rukshanlancer@gmail.com>
2023-07-01 01:41:42 +0800

    final

--- js/action.js
+++ js/action.js
@@ -1,13 +1,7 @@
 var leftCornerButton = document.getElementById("left-corner");
 var footer = document.getElementById("footer");
-// var leftCorner = document.getElementById("left-corner");
 
 leftCornerButton.addEventListener("click", function () {
   footer.classList.toggle("shrunk");
   leftCornerButton.classList.toggle("flip");
-  // if (footer.classList.contains("shrunk")) {
-  //   leftCorner.style.left = "0px";
-  // } else {
-  //   leftCorner.style.left = "20px";
-  // }
 });
