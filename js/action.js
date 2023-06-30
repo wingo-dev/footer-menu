bad213a6c17d8717e7add966684b23bf8ed2197c
wingo-dev <rukshanlancer@gmail.com>
2023-06-30 14:49:17 +0800

    remove the from right to left effect

--- js/action.js
+++ js/action.js
@@ -1,13 +1,13 @@
 var leftCornerButton = document.getElementById("left-corner");
 var footer = document.getElementById("footer");
-var leftCorner = document.getElementById("left-corner");
+// var leftCorner = document.getElementById("left-corner");
 
 leftCornerButton.addEventListener("click", function () {
   footer.classList.toggle("shrunk");
   leftCornerButton.classList.toggle("flip");
-  if (footer.classList.contains("shrunk")) {
-    leftCorner.style.left = "0px";
-  } else {
-    leftCorner.style.left = "20px";
-  }
+  // if (footer.classList.contains("shrunk")) {
+  //   leftCorner.style.left = "0px";
+  // } else {
+  //   leftCorner.style.left = "20px";
+  // }
 });
